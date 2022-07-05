4f4e35d5b43e61d607a4c7c75c5aa95fb4d44d58
Allchymist <pedrohenry015@gmail.com>
2022-07-05 16:18:11 -0300

    Add Cors and EnsureAuth in Admin Router

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,24 @@
 const { Router } = require('express');
+const cors = require('cors');
 
 const userRouter = require('./user.routes');
 const adminRouter = require('./admin.routes');
 
+const ensureAuth = require('../middlewares/ensureAuth');
+
 const routes = Router();
 
+const optionsCors = {
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Toke', 'Authorization'],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: '*',
+  preflightContinue: false,
+}
+
+routes.use(cors(optionsCors));
+
 routes.use('/auth', userRouter);
-routes.use('/admin', adminRouter);
+routes.use('/admin', ensureAuth, adminRouter);
 
 module.exports = routes;

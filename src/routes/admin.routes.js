06af0e7c3611e1085eb00ee2b2098b123cbeb6df
Allchymist <pedrohenry015@gmail.com>
2022-07-05 16:18:44 -0300

    Add Profile router

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -4,8 +4,10 @@ const AdminController = require('../controllers/admin/admin-controller');
 
 const adminRouter = Router();
 
-adminRouter.put('/register', AdminController.register);
+adminRouter.get('/profile/:id', AdminController.profile);
+
 adminRouter.get('/user/:id/details', AdminController.profile);
+adminRouter.put('/user/register', AdminController.register);
 adminRouter.patch('/user/:id/edit', AdminController.editProfile);
 adminRouter.delete('/user/:id', AdminController.deleteProfile);
 
